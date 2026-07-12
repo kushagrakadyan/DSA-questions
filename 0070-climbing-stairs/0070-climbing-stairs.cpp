e85bd3e413152646d0bcc9734fc6b5c57d4f964a
kushagrakadyan <kushagrakadyan@gmail.com>
2026-07-12 23:49:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-    //int count(int n)
-    int count(int n,vector<int> &dp){
-        if(n==1 || n==2 ){
+    //memoisation se
+
+    // int count(int n,vector<int> &dp){
+    //     if(n==1 || n==2 ){
+    //         return n;
+    //     }
+    //     if(dp[n] != -1) return dp[n];
+    //     return dp[n]=count(n-1,dp)+count(n-2,dp);
+    // }    
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1,-1); // 1 ke baad se suru hone ke liye
+    //     return count(n,dp);
+    // }
+
+
+    //tablulation se
+    int climbStairs(int n){
+        if(n==1){
             return n;
         }
-        if(dp[n] != -1) return dp[n];
-        return dp[n]=count(n-1,dp)+count(n-2,dp);
-    }    
-    int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return count(n,dp);
+        vector<int> dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
