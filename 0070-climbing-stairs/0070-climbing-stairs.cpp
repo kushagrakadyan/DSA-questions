137ea908d65a4b0219716ba87fee19a4ac256f67
kushagrakadyan <kushagrakadyan@gmail.com>
2026-08-11 21:46:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -17,10 +17,8 @@ public:
 
     //tablulation se
     int climbStairs(int n){
-        if(n==1){
-            return n;
-        }
-        vector<int> dp(n+1);
+        if(n<=2) return n;
+        vector<int>dp(n+1);
         dp[1]=1;
         dp[2]=2;
         for(int i=3;i<=n;i++){
